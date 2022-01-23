7e486ef3f796c4cad75efc1cc9d6d9da8863d385
danic-bit <danielaarquitectonica@gmail.com>
2022-01-23 15:25:12 -0300

    Commit inicial estructura basica

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+$('body').on('dblclick', '#ingredientes', '#preparacion', function(){
+    $(this).css("color","red")
+})
+
+$("#enviarCorreo").click(function(){
+alert("El correo fue enviado correctamente")
+})
